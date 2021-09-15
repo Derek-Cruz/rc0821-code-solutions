acd580ee1849c90448fdec4639be78f66e8481ef
Derek-Cruz <Derekcruz94@gmail.com>
2021-09-14 17:58:04 -0700

    Completed javascript constructors.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,29 @@
+var aObject = new Object();
+
+console.log('value of aObject:', aObject);
+console.log('typeof aObject:', typeof aObject);
+
+var aArray = new Array();
+
+console.log('value of aArray:', aArray);
+console.log('typeof aArray:', typeof aArray);
+
+var aFunction = new Function();
+
+console.log('value of aFunction:', aFunction);
+console.log('typeof aFunction:', typeof aFunction);
+
+var aNumber = new Number();
+
+console.log('value of aNumber:', aNumber);
+console.log('typeof aNumber:', typeof aNumber);
+
+var aBoolean = new Boolean();
+
+console.log('value of aBoolean:', aBoolean);
+console.log('typeof aBoolean:', typeof aBoolean);
+
+var aString = new String();
+
+console.log('value of aString:', aString);
+console.log('typeof aString:', typeof aString);
