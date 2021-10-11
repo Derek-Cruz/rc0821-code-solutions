3b031f86debb7a383753a160d357c7da0643c227
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-11 12:13:11 -0700

    In progress with getting ID.

---
+++ express-notes-api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+// const fs = require('fs');
+const notes = require('./data.json');
+
+const parse = express.json();
+app.use(parse);
+
+app.get('/api/notes', (req, res) => {
+  const notesArr = [];
+  for (const prop in notes.notes) {
+    const resJSON = notes.notes[prop];
+    notesArr.push(resJSON);
+  }
+  res.status(200).json(notesArr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (Number.isInteger(parseFloat(req.params.id)) && parseFloat(req.params.id) > 0) {
+    if (req.params.id in notes.notes) {
+      res.status(200).json(notes.notes[req.params.id]);
+    } else {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+  } else {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on Port 3000');
+});
