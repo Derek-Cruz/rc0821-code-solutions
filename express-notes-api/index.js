f669c43d598370e117e886213740e76ae0f9fe00
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-11 16:23:13 -0700

    Completed express notes api.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -16,7 +16,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  if (Number.isInteger(parseFloat(req.params.id)) && parseFloat(req.params.id) > 0) {
+  if (Number.isInteger(parseInt(req.params.id)) && parseInt(req.params.id) > 0) {
     if (req.params.id in notes.notes) {
       res.status(200).json(notes.notes[req.params.id]);
     } else {
@@ -48,7 +48,7 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete('/api/notes/:id', (req, res) => {
-  if (Number.isInteger(parseFloat(req.params.id)) && parseFloat(req.params.id) > 0) {
+  if (Number.isInteger(parseInt(req.params.id)) && parseInt(req.params.id) > 0) {
     if (req.params.id in notes.notes) {
       delete notes.notes[req.params.id];
       fs.writeFile('./data.json', JSON.stringify(notes, null, 2), err => {
@@ -62,6 +62,30 @@ app.delete('/api/notes/:id', (req, res) => {
       res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
     }
   } else {
+    res.status(400).json({ error: 'id must be a positive integer' });
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  if (Number.isInteger(parseInt(req.params.id)) && parseInt(req.params.id) > 0 && req.body.content !== undefined) {
+    if (req.params.id in notes.notes) {
+      notes.notes[req.params.id] = {
+        id: req.params.id,
+        content: req.body.content
+      };
+      fs.writeFile('./data.json', JSON.stringify(notes, null, 2), err => {
+        if (err) {
+          res.status(500).json({ error: 'An unexpected error has occurred' });
+        } else {
+          res.status(200).json(notes.notes[req.params.id]);
+        }
+      });
+    } else {
+      res.status(404).json({ error: `cannot find note with id ${req.params.id}` });
+    }
+  } else if (req.body.content === undefined) {
+    res.status(400).json({ error: 'content is a required field' });
+  } else if (req.body.content !== undefined) {
     res.status(400).json({ error: 'id must be a positive integer' });
   }
 });
