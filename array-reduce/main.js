49f48d8d36c65c83f33893de08145326a711cf2e
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-15 12:33:46 -0700

    Finished sum.

---
+++ array-reduce/main.js
@@ -0,0 +1,31 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = numbers.reduce((num1, num2) => num1 + num2);
+console.log('results of sum:', sum);
+
+const product = numbers.reduce((num1, num2) => num1 * num2);
+console.log('results of product:', product);
+
+const balance = account.reduce((test1, test2) => (
+  (test2.type === 'deposit')
+    ? test1 + test2.amount
+    : test1 - test2.amount
+), 0);
+console.log('results of balance:', balance);
