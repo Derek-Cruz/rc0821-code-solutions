b909063ab911976e2874834b361e71f7851baad2
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-20 21:53:09 -0700

    trying to make a circle show.

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
-class StopWatch extends React.Component {}
+class StopWatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: false,
+      time: 0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='circle'>{this.state.time}</div>
+      </div>
+    );
+  }
+}
 
 export default StopWatch;
