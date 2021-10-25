d8c03c53ea6d1c20e375213d9d9a60ede5971cae
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-25 12:52:09 -0700

    Completed Fetch.

---
+++ fetch/main.js
@@ -0,0 +1,11 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(data => {
+    console.log('users:', data);
+  });
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(response => response.json())
+  .then(data => {
+    console.log('info:', data);
+  });
