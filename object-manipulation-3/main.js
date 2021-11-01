5f042bd0d6d294832258938475d5b9646ec3312f
Derek-Cruz <Derekcruz94@gmail.com>
2021-10-31 22:22:31 -0700

    Reworking obj-man3.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,57 +1,70 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
+const deck = [];
 const players = [
   {
     name: 'Derek Cruz',
-    hand: 0
+    hand: [],
+    score: null
   },
   {
     name: 'Brett Albright',
-    hand: 0
+    hand: [],
+    score: null
   },
   {
     name: 'Scott Bowler',
-    hand: 0
+    hand: [],
+    score: null
   },
   {
     name: 'Tim Horist',
-    hand: 0
+    hand: [],
+    score: null
   }
 ];
 
-const suits = ['Spades', 'Diamonds', 'Club', 'Heart'];
-const values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-const deck = [];
-
-for (let suitsIndex = 0; suitsIndex < suits.length; suitsIndex++) {
-  for (let valuesIndex = 0; valuesIndex < values.length; valuesIndex++) {
-    const card = { Value: values[valuesIndex], Suit: suits[suitsIndex] };
-    deck.push(card);
+function createDeck(type) {
+  for (let deckSort = 1; deckSort < 11; deckSort++) {
+    deck.push({ rank: deckSort, suit: type });
   }
+  deck.push({ rank: 'A', suit: type });
+  deck.push({ rank: 'K', suit: type });
+  deck.push({ rank: 'Q', suit: type });
+  deck.push({ rank: 'J', suit: type });
 }
 
-for (let getCard = deck.length - 1; getCard > 0; getCard--) {
-  const testing = Math.floor(Math.random() * getCard);
-  const temp = deck[getCard];
-  deck[getCard] = deck[testing];
-  deck[testing] = temp;
-}
+createDeck('spades');
+createDeck('diamonds');
+createDeck('hearts');
+createDeck('clubs');
 
-function dealCards(cards) {
-  for (let dealCard = 0; dealCard < players.length; dealCard++) {
-    players[dealCard].hand = deck.splice(0, 2);
-    // console.log('test', dealCards(players[dealCard].hand));
+const shuffled = _.shuffle(deck);
 
+let testing = 0;
+for (let i = 0; i < players.length; i++) {
+  for (let handIndex = testing; handIndex <= testing + 1; handIndex++) {
+    players[i].hand.push(shuffled[handIndex]);
   }
+  testing += 2;
 }
 
-dealCards(players.hand);
-// console.log('test', dealCards(players.hand));
+// const score = 0;
 
-// function getScores(handValue) {
-//   for (var i = 0; i < players.length; i++) {
-// /
-//   }
+// function getFirstValue(rank) {
+//   (rank === 'J' || rank === 'K' || rank === 'Q')
+//     ? score = 10
+//     : (rank === 'A')
+//         ? score = 11
+//         : score = rank;
+//   return score;
 // }
-// var result = getScores(players[i].hand);
-// players[].playerScore =
+//
+// function getSecondValue(rank) {
+//   (rank === 'J' || rank === 'K' || rank === 'Q')
+//     ? score = 10
+//     : (rank === 'A')
+//         ? score = 11
+//         : score = rank;
+//   return score;
+// }
