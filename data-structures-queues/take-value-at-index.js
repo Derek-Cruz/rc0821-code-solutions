4efe6f39408fe5307e7691e0ea94b9c4efe4ac67
Derek-Cruz <Derekcruz94@gmail.com>
2021-11-16 20:04:22 -0800

    Completed data structures queues.

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,15 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
-
+  let front = queue.dequeue();
+  let cycle = index;
+  if (typeof front === 'undefined') {
+    return;
+  }
+  while (cycle > 0) {
+    queue.enqueue(front);
+    front = queue.dequeue();
+    cycle--;
+  }
+  return front;
 }
